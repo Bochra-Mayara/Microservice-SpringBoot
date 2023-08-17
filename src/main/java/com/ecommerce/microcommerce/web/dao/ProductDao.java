a71154de99ac317c5defebf7fa40366f120ddf88
bochra-mayara <bochra-mayara>
2023-08-17 17:20:16 +0200

    first commit

--- src/main/java/com/ecommerce/microcommerce/web/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/web/dao/ProductDao.java
@@ -1,12 +1,11 @@
 package com.ecommerce.microcommerce.web.dao;
 
 import com.ecommerce.microcommerce.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface ProductDao extends JpaRepository<Product, Integer> {
+public interface ProductDao {
+    List<Product> findAll();
     Product findById(int id);
+    Product save(Product product);
 }
